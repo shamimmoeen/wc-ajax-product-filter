9d7478cc2c2a80e0096c49da409f2bf73eb7a98a
Mainul Hassan Main <shamim.moeen@gmail.com>
2022-10-23 13:30:50 +0600

    Move the edit-filter element inside the main element

--- build/filter.asset.php
+++ build/filter.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-notices', 'wp-polyfill', 'wp-primitives'), 'version' => 'cf0b2cdb72f985400afd');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-notices', 'wp-polyfill', 'wp-primitives'), 'version' => '7e5c7ea4f26405d70c06');
