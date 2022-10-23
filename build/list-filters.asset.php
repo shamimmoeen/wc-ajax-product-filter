6e58bcfd04279928af58866943300600e76771ae
Mainul Hassan Main <shamim.moeen@gmail.com>
2022-10-23 13:24:52 +0600

    Improve the top bar design

--- build/list-filters.asset.php
+++ build/list-filters.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'ab5158b8a8c9bd7a4920');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '88fc5f09803f775517fe');
