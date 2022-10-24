b48670dfddb94a53f06bcf510a686d06b992e691
Mainul Hassan Main <shamim.moeen@gmail.com>
2022-10-25 01:13:36 +0600

    Modify the pro tag gradient

--- build/list-filters.asset.php
+++ build/list-filters.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'df17a698593943a3fb30');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '5a79d3e177541e7fab5a');
