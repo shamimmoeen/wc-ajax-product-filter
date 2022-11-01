cae706c27557c8e0d0f05a603892d9f318bc2df2
Mainul Hassan Main <shamim.moeen@gmail.com>
2022-11-01 19:50:41 +0600

    Modify the list filters table columns

--- build/list-filters.asset.php
+++ build/list-filters.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '2485ef7915f1f5125cd4');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7899f3b09aca96ec17ed');
