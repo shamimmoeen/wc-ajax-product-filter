4c07d7d71655cb56f8d3e834a6a64c042bce0f44
Mainul Hassan Main <shamim.moeen@gmail.com>
2022-11-06 12:21:02 +0600

    Improve the design of visibility rules section

--- build/form.asset.php
+++ build/form.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '1cbcab5c79b96c1ee9e5');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'c1b966b5d39de2c344e2');
