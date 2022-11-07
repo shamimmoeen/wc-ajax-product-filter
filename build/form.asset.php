02acd88033d98f4e00114b067b981b59a74fef0f
Mainul Hassan Main <shamim.moeen@gmail.com>
2022-11-08 00:17:51 +0600

    Remove the alternate save icon

--- build/form.asset.php
+++ build/form.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'bda38d4e18d07f735227');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '22a299108776216bdf26');
--- build/settings.asset.php
+++ build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n'), 'version' => '41b2039e08dccc51691c');
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a69290803800407265c9');
