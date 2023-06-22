000a0f9aa1bcfd68aa329cf759b768d4ec70685b
Mainul Hassan Main <shamim.moeen@gmail.com>
2023-06-22 11:25:21 +0600

    Add icons for 'List of Forms' and 'Settings' cards

--- build/list-forms.asset.php
+++ build/list-forms.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '16fb55e9dd3a197664f4');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'e1db8b6eec75665e7955');
